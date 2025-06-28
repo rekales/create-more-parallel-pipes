b7c379930aa588a22723feb9b92054463294a711
Krei <50932886+rekales@users.noreply.github.com>
2025-06-28 20:05:29 +0800

    temp fix for copycats+ and tfmg pipes version bump to 1.0.2

--- src/main/java/com/krei/cmparallelpipes/CommonHandler.java
+++ src/main/java/com/krei/cmparallelpipes/CommonHandler.java
@@ -1,5 +1,6 @@
 package com.krei.cmparallelpipes;
 
+import com.simibubi.create.AllBlocks;
 import com.simibubi.create.content.fluids.pipes.FluidPipeBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionResult;
@@ -19,6 +20,7 @@ public class CommonHandler {
 
         if (!event.getEntity().isCrouching()
                 && event.getItemStack().getItem() instanceof ScaffoldingBlockItem
+                && AllBlocks.FLUID_PIPE.has(blockState) // NOTE: Temp Fix
                 && blockState.getBlock() instanceof FluidPipeBlock) {
             if (!level.isClientSide()
                     && !(blockState.getBlock() instanceof LockedFluidPipeBlock)) {
