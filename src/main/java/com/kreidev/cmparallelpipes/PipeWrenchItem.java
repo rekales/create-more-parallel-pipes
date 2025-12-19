1fdbb7daad4f938c009dbbbc71af11dceef3c321
Krei <criketgun@gmail.com>
2025-12-19 17:06:29 +0800

    fix connections sometimes happening even locked

--- src/main/java/com/kreidev/cmparallelpipes/PipeWrenchItem.java
+++ src/main/java/com/kreidev/cmparallelpipes/PipeWrenchItem.java
@@ -1,12 +1,17 @@
 package com.kreidev.cmparallelpipes;
 
+import com.simibubi.create.AllBlocks;
+import com.simibubi.create.content.fluids.FluidPropagator;
 import com.simibubi.create.content.fluids.pipes.FluidPipeBlockEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 
 public class PipeWrenchItem extends Item {
@@ -17,6 +22,8 @@ public class PipeWrenchItem extends Item {
     @Override
     public @NotNull InteractionResult onItemUseFirst(@NotNull ItemStack stack, UseOnContext context) {
         Level level = context.getLevel();
+        BlockPos pos = context.getClickedPos();
+        BlockState blockState = level.getBlockState(pos);
 
         if (level.getBlockEntity(context.getClickedPos()) instanceof FluidPipeBlockEntity pipeBlockEntity) {
                 Player player = context.getPlayer();
@@ -26,6 +33,14 @@ public class PipeWrenchItem extends Item {
                     pipeBlockEntity.setData(ParallelPipes.LOCKED_DATA_ATTACHMENT.get(), false);
                     if (level.isClientSide()) {  // NOTE: For instant feedback
                         ClientHandler.renderedBlockEntities.remove(pipeBlockEntity);
+                    } else {
+                        Direction.Axis preferred = FluidPropagator.getStraightPipeAxis(blockState);
+                        Direction preferredDirection =
+                                preferred == null ? Direction.UP : Direction.get(Direction.AxisDirection.POSITIVE, preferred);
+                        BlockState updated = AllBlocks.FLUID_PIPE.get()
+                                .updateBlockState(blockState, preferredDirection, null, level, pos);
+                        if (updated != blockState)
+                            level.setBlockAndUpdate(pos, updated);
                     }
                 } else {
                     pipeBlockEntity.setData(ParallelPipes.LOCKED_DATA_ATTACHMENT.get(), true);
--- src/main/java/com/kreidev/cmparallelpipes/mixin/FluidPipeBlockMixin.java
+++ src/main/java/com/kreidev/cmparallelpipes/mixin/FluidPipeBlockMixin.java
@@ -13,22 +13,10 @@ import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = FluidPipeBlock.class, remap = false)
 public abstract class FluidPipeBlockMixin {
-
-    @ModifyVariable(method = "updateBlockState", at = @At("STORE"), name = "shouldConnect")
-    private boolean alterShouldConnect(boolean value, BlockState state, Direction preferredDirection, @Nullable Direction ignore,
-                                       BlockAndTintGetter world, BlockPos pos) {
-        // Cancel self update when locked
-        if (world.getBlockEntity(pos) instanceof FluidPipeBlockEntity pipeBlockEntity
-                && pipeBlockEntity.getData(ParallelPipes.LOCKED_DATA_ATTACHMENT.get())) {
-            return false;
-        }
-        return value;
-    }
 
     @Inject(method = "canConnectTo", at = @At("HEAD"), cancellable = true)
     private static void canConnectTo(BlockAndTintGetter world, BlockPos neighbourPos, BlockState neighbour,
@@ -42,6 +30,17 @@ public abstract class FluidPipeBlockMixin {
             } else {
                 cir.setReturnValue(transport.canHaveFlowToward(neighbour, direction.getOpposite()));
             }
+            cir.cancel();
+        }
+    }
+
+    @Inject(method = "updateBlockState", at = @At("HEAD"), cancellable = true)
+    public void updateBlockState(BlockState state, Direction preferredDirection, @Nullable Direction ignore,
+                                       BlockAndTintGetter world, BlockPos pos, CallbackInfoReturnable<BlockState> cir) {
+        // Cancel self update when locked
+        if (world.getBlockEntity(pos) instanceof FluidPipeBlockEntity pipeBlockEntity
+                && pipeBlockEntity.getData(ParallelPipes.LOCKED_DATA_ATTACHMENT.get())) {
+            cir.setReturnValue(state);
             cir.cancel();
         }
     }
